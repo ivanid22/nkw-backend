6a41b1d88ceda1cb08b47173eb2e7151e6d09f73
Ivan Diaz <ivanid22@gmail.com>
2020-11-17 13:49:04 -0300

    Add registrations controller callback to create UserProfile on account registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  #mount_devise_token_auth_for 'User', at: 'auth'
+
+  mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+    confirmations:      'devise_token_auth/confirmations',
+    passwords:          'devise_token_auth/passwords',
+    omniauth_callbacks: 'devise_token_auth/omniauth_callbacks',
+    registrations:      'registrations_callback',
+    sessions:           'devise_token_auth/sessions',
+    token_validations:  'devise_token_auth/token_validations'
+  }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   scope '/v1' do
