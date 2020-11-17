15ecf1f121058ad6b54bcc98bc3fcaf251fbabcd
Ivan Diaz <ivanid22@gmail.com>
2020-11-17 13:26:12 -0300

    Add trackable fields to user schema

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,10 @@
 class FavoritesController < ApplicationController
+  def create
+    @user_profile = UserProfile.find(params[:user_profile_id])
+    @
+  end
+
+  def favorite_params
+    params.require(:favorite).permit(:user_profile_id, :posting_id)
+  end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,8 @@
 class Favorite < ApplicationRecord
   belongs_to :user_profile, class_name: "UserProfile", foreign_key: "user_profile_id"
   belongs_to :posting, class_name: "Posting", foreign_key: "posting_id"
+
+  def self.favorite_exists?(user_profile, posting)
+    !Favorite.where(user_profile: user_profile, posting: posting).empty?
+  end
 end
