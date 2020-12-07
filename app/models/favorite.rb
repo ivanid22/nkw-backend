586503b8dcf46c7023a444014ce110635ebdc1ac
Ivan Diaz <ivanid22@gmail.com>
2020-12-07 16:57:14 -0300

    Removed unnecessary class names for static methods favorites_for, favorites_by, and exists? on model Favorite

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -3,14 +3,14 @@ class Favorite < ApplicationRecord
   belongs_to :posting, class_name: 'Posting', foreign_key: 'posting_id'
 
   def self.exists?(user_profile_id, posting_id)
-    !Favorite.where(user_profile_id: user_profile_id, posting_id: posting_id).empty?
+    !where(user_profile_id: user_profile_id, posting_id: posting_id).empty?
   end
 
   def self.favorites_by(user_profile_id)
-    Favorite.where(user_profile_id: user_profile_id)
+    where(user_profile_id: user_profile_id)
   end
 
   def self.favorites_for(posting_id)
-    Favorite.where(posting_id: posting_id)
+    where(posting_id: posting_id)
   end
 end
